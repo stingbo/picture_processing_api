dcca2c8670ba13056b0fe19290b5b679c7206296
wanlianbo <lianbo.wan@tiantian8.com>
2015-12-28 21:59:46 +0800

    增加其它请求方式

--- index.php
+++ index.php
@@ -9,5 +9,20 @@ $app->get('/index/{name}', function ($request, $response, $args) {
     return $response->write("Hello " . $args['name']);
 });
 
+$app->post('/index/{name}', function ($request, $response, $args) {
+    //return $response->write("Hello " . $args['name']);
+    echo 'This is a POST route';
+});
+
+$app->put('/index/{name}', function ($request, $response, $args) {
+    //return $response->write("Hello " . $args['name']);
+    echo 'This is a PUT route';
+});
+
+$app->delete('/index/{name}', function ($request, $response, $args) {
+    //return $response->write("Hello " . $args['name']);
+    echo 'This is a DELETE route';
+});
+
 // Run app
 $app->run();
